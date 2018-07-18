88737c71446af935c6f103a963f3f2ea0b7a580e
Michael <36787351+plushscumichael@users.noreply.github.com>
2018-07-18 18:50:39 +0300

    Add files via upload

--- src/components/NoteDetails.vue
+++ src/components/NoteDetails.vue
@@ -1,10 +1,11 @@
 <template>
-      <form @submit.prevent = 'changeThisNote' >
-        <label class = 'note-details'>
-          <textarea class = 'note-textarea' v-model='noteText'> </textarea>
-          <button class="create-a-note-button">Change</button>
-        </label>
-      </form>
+    <form @submit.prevent = 'changeThisNote' class="note-details-form" >
+      <label class = 'note-details'>
+        <div class = 'current-note'>{{this.notes[this.notes.findIndex(elem => String(elem.id) === String(this.$route.params.id))].texts}}</div>
+        <textarea class = 'note-textarea' v-model='noteText'> </textarea>
+      </label>
+      <button class="note-details-button">Change</button>
+    </form>
 </template>
 
 <script>
@@ -36,15 +37,17 @@
 
 <style>
 
-  .note-details{
+  .note-details-form{
     display:flex;
+    width: 100%;
     flex-direction: column;
     align-items: center;
+  }
+
+  .note-details{
+    display:flex;
     border: 1px solid white;
     background-color: white;
-    width: 300px;
-    height: 300px;
-    margin: 10px;
     color: black;
     font-size: 20px;
   }
@@ -52,10 +55,35 @@
   .note-textarea{
     border: 1px solid #0f76b3;
     outline: none;
-    width: 297px;
-    height: 240px;
+    width: 300px;
+    height: 300px;
     resize:none;
     font-size: 20px;
+    margin: 20px;
+  }
+
+  .current-note{
+    border: 1px solid #0f76b3;
+    width: 300px;
+    height: 300px;
+    color: black;
+    font-size: 20px;
+    text-align: center;
+    margin: 20px;
+  }
+
+  .note-details-button{
+    padding: 15px 200px;
+    border: 1px solid #0f76b3;
+    font-size: 16px;
+    color: #0f76b3;
+    background-color: #fff;
+    transition: color .5s ease, background-color .3s ease;
+  }
+
+  .note-details-button:hover{
+    background-color: #118dd4;
+    color: #fff;
   }
 
 </style>
