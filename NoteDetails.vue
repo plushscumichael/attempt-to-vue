8a304823844a89826adbd1b3d240cb6b06472e3c
Michael <36787351+plushscumichael@users.noreply.github.com>
2018-07-20 19:44:11 +0300

    Add files via upload

---
+++ NoteDetails.vue
@@ -0,0 +1,59 @@
+<template>
+  <label class="note-details-form">
+    <textarea class = 'note-input'
+              :value="notes[this.notes.findIndex(elem => String(elem.id) === String(this.$route.params.id))].texts"
+              @input="noteText = $event.target.value"
+              @keydown.enter="changeThisNote"></textarea>
+  </label>
+</template>
+
+<script>
+  import Note from './Note.vue'
+  import NoteKeep from '../js/NoteKeep.js'
+
+  export default{
+    components:{
+      Note
+    },
+    data:() => ({
+      notes: [],
+      noteText:''
+    }),
+    methods:{
+      changeThisNote(){
+        const index = this.notes.findIndex(elem => String(elem.id) === String(this.$route.params.id));
+        this.notes[index].texts = this.noteText
+        localStorage.setItem('note-storage', JSON.stringify(this.notes));
+        this.$router.push('/');
+        this.$emit('changeThisNote')
+      }
+    },
+    created(){
+      this.notes = NoteKeep.notes;
+    }
+  }
+</script>
+
+<style>
+
+  .note-details-form{
+    display:flex;
+    width: 100%;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .note-input{
+    border: 1px solid dimgray;
+    width: 300px;
+    height: 300px;
+    resize:none;
+    outline: none;
+    font-size: 20px;
+    color: black;
+    margin: 5%;
+    font-family: 'Montserrat', sans-serif;
+    text-align:center;
+  }
+
+</style>
